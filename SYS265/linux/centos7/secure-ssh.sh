2b9513985090dd7c1289b9b31512a7a09c238ee1
samuel-richardson <samuel.richardson@mymail.champlain.edu>
2023-02-16 11:41:48 -0500

    secure.ssh starter file

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#Author sam richardson
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#remove roots ability to ssh in
+echo "ALL YOUR CODE GOES HERE"
