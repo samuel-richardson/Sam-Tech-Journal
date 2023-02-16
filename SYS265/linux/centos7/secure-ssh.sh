59e69bc95ec2a22b9d09db14bcd030460883ffcc
samuel-richardson <samuel.richardson@mymail.champlain.edu>
2023-02-16 12:03:58 -0500

    Added public key

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -1,6 +1,17 @@
 #secure-ssh.sh
 #Author sam richardson
+
 #creates a new ssh user using $1 parameter
+$user = $1
+sudo useradd -m -d /home/$user -s /bin/bash
+sudo mkdir /home/$user/.ssh
+sudo cp SYS265/linux/public-keys/id_rsa.pub /home/$user/.ssh/authroized_keys
+sudo chmod 700 /home/$user/.ssh
+sudo chmod 600 /home/$user/.ssh/authorized_keys
+sudo chown R $user:$user /home/$user/.ssh
+sudo usermod -aG wheel,adm,sudo $user
+
+
 #adds a public key from the local repo or curled from the remote repo
 #remove roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE"
+
