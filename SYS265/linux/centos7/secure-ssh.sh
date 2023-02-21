4e160fcadded7f489ff6aba73db37717f3689beb
samuel-richardson <samuel.richardson@mymail.champlain.edu>
2023-02-21 23:50:32 +0000

    Updated secure_ssh

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -1,17 +1,23 @@
+#!/bin/bash
 #secure-ssh.sh
 #Author sam richardson
 
-#creates a new ssh user using $1 parameter
-$user = $1
-sudo useradd -m -d /home/$user -s /bin/bash
-sudo mkdir /home/$user/.ssh
-sudo cp SYS265/linux/public-keys/id_rsa.pub /home/$user/.ssh/authroized_keys
-sudo chmod 700 /home/$user/.ssh
-sudo chmod 600 /home/$user/.ssh/authorized_keys
-sudo chown R $user:$user /home/$user/.ssh
-sudo usermod -aG wheel,adm,sudo $user
-
-
 #adds a public key from the local repo or curled from the remote repo
 #remove roots ability to ssh in
+#creates a new ssh user using $1 parameter
+$1
+
+sudo useradd -m -d /home/$1 -s /bin/bash $1
+sudo mkdir /home/$1/.ssh
+sudo cp ../public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
+sudo chmod 700 /home/$1/.ssh
+sudo chmod 600 /home/$1/.ssh/authorized_keys
+sudo chown -R $1:$1 /home/$1/.ssh
+sudo usermod -aG adm,sudo $1
+
+sudo echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+sudo systemctl restart sshd
+
+sudo systemctl restart sshd
+
 
