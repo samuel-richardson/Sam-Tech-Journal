94d33b58d34aeb2782efb1657c78e3c922aa0f32
samuel-richardson <89803620+samuel-richardson@users.noreply.github.com>
2023-02-21 19:10:16 -0500

    Update secure-ssh.sh

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -5,7 +5,6 @@
 #adds a public key from the local repo or curled from the remote repo
 #remove roots ability to ssh in
 #creates a new ssh user using $1 parameter
-$1
 
 sudo useradd -m -d /home/$1 -s /bin/bash $1
 sudo mkdir /home/$1/.ssh
@@ -16,8 +15,6 @@ sudo chown -R $1:$1 /home/$1/.ssh
 sudo usermod -aG adm,sudo $1
 
 sudo echo "PermitRootLogin no" >> /etc/ssh/sshd_config
-sudo systemctl restart sshd
-
 sudo systemctl restart sshd
 
 
